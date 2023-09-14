50525aea9492bb41f45082526d52781a2ecd3cda
ThibaultAncele <t.ancele@net.estia.fr>
2023-09-14 11:42:42 +0200

    Création d'un premier hook state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
+import Pokemon from './models/pokemon';
+import POKEMONS from './models/mock-pokemon';
   
 const App: FunctionComponent = () => {
- const name: String = 'React';
+
+ const [pokemons] = useState<Pokemon[]>(POKEMONS)
     
  return (
-  <h1>Hello, {name} !</h1>
+  <div>
+    <h1>Pokédex</h1>
+    <p>Il y a {pokemons.length} dans notre application !</p>
+  </div>
+
  )
 }
   
 export default App;
+
+/*
+import React from 'react';
+
+export default class App extends React.Component {
+    const name: string ="React";
+
+    render() {
+        return <h1>Bonjour, {name}</h1>;
+    }
+}
+*/
